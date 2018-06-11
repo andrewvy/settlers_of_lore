6ed7a0806562405bb724bc351d5527b194305353
Andrew Vy <andrew@andrewvy.com>
2018-06-11 13:06:34 -0700

    :sparkles: Switch to ggez
    This commit switches from piston libraries to mainly ggez/sdl2. This also implements a very basic hover button.

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,170 @@
-extern crate find_folder;
-extern crate freetype;
-extern crate graphics;
-extern crate opengl_graphics;
-extern crate piston;
-extern crate sdl2_window;
+extern crate ggez;
 
-use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventLoop, EventSettings, Events};
-use piston::input::*;
-use piston::window::WindowSettings;
-use sdl2_window::Sdl2Window as Window;
+use ggez::conf::{WindowMode, WindowSetup};
+use ggez::event::{self, Keycode, Mod, MouseState};
+use ggez::graphics::{self, Color, DrawParam, Font, HorizontalAlign as HAlign, Layout, Point2,
+                     Rect, Scale, TextCached, TextFragment};
+use ggez::timer;
+use ggez::{Context, ContextBuilder, GameResult};
 
-mod glyph_text;
+use std::collections::BTreeMap;
+use std::env;
+use std::f32;
+use std::path;
 
-use glyph_text::GlyphText;
+struct MainState {
+    texts: BTreeMap<&'static str, TextCached>,
+    button: Button,
+    mouse_x: i32,
+    mouse_y: i32,
+}
+
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let mut texts = BTreeMap::new();
+
+        /*
+        let font = Font::new_glyph_font(ctx, "/m5x7.ttf")?;
+
+        let mut text = TextCached::new(TextFragment {
+            text: "Hello, world!".to_string(),
+            font_id: Some(font.clone().into()),
+            scale: Some(Scale::uniform(48.0)),
+            ..Default::default()
+        })?;
+
+        text.set_bounds(
+            Point2::new(300.0, f32::INFINITY),
+            Some(Layout::default().h_align(HAlign::Center)),
+        );
+
+        texts.insert("0_hello", text);
+        */
+
+        Ok(MainState {
+            texts,
+            mouse_x: 0,
+            mouse_y: 0,
+            button: Button {
+                color: Color::new(0.5, 0.5, 0.5, 1.0),
+                rectangle: Rect {
+                    x: 50.0,
+                    y: 150.0,
+                    w: 150.0,
+                    h: 50.0,
+                },
+                hovered: false,
+            },
+        })
+    }
+}
+
+struct Button {
+    rectangle: Rect,
+    color: Color,
+    hovered: bool,
+}
+
+impl Button {
+    fn update(&mut self, mouse_x: i32, mouse_y: i32) {
+        if (((mouse_x as f32) > self.rectangle.x)
+            && ((mouse_x as f32) < self.rectangle.x + self.rectangle.w))
+            && (((mouse_y as f32) > self.rectangle.y)
+                && ((mouse_y as f32) < self.rectangle.y + self.rectangle.h))
+        {
+            self.hovered = true;
+        } else {
+            self.hovered = false;
+        }
+    }
+
+    fn render(&self, ctx: &mut Context) -> GameResult<()> {
+        graphics::set_color(ctx, self.color)?;
+
+        let mut draw_mode = graphics::DrawMode::Line(1.0);
+
+        if self.hovered {
+            draw_mode = graphics::DrawMode::Fill;
+        }
+
+        graphics::rectangle(ctx, draw_mode, self.rectangle)
+    }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        const DESIRED_FPS: u32 = 60;
+        while timer::check_update_time(ctx, DESIRED_FPS) {}
+
+        self.button.update(self.mouse_x, self.mouse_y);
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+
+        let fps = timer::get_fps(ctx);
+        let fps_display = TextCached::new(format!("FPS: {}", fps))?;
+
+        let mouse_coords = TextCached::new(format!("x: {}, y: {}", self.mouse_x, self.mouse_y))?;
+
+        graphics::draw(ctx, &fps_display, Point2::new(0.0, 0.0), 0.0)?;
+        graphics::draw(ctx, &mouse_coords, Point2::new(0.0, 20.0), 0.0)?;
+
+        let mut height = 0.0;
+        for (_key, text) in &self.texts {
+            text.queue(ctx, Point2::new(0.0, 20.0 + height), None);
+            height += 20.0 + text.height(ctx) as f32;
+        }
+
+        TextCached::draw_queued(ctx, DrawParam::default())?;
+
+        self.button.render(ctx)?;
+
+        graphics::present(ctx);
+        timer::yield_now();
+
+        Ok(())
+    }
+
+    fn mouse_motion_event(
+        &mut self,
+        _ctx: &mut Context,
+        _state: MouseState,
+        x: i32,
+        y: i32,
+        _xrel: i32,
+        _yrel: i32,
+    ) {
+        self.mouse_x = x;
+        self.mouse_y = y;
+    }
+
+    fn key_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        _keycode: Keycode,
+        _keymod: Mod,
+        _repeat: bool,
+    ) {
+    }
+}
 
 fn main() {
-    let opengl = OpenGL::V3_2;
-    let mut window: Window = WindowSettings::new("piston-example-freetype", [1024, 720])
-        .resizable(false)
-        .opengl(opengl)
-        .samples(2)
+    let ctx = &mut ContextBuilder::new("settlers_of_lore", "vy")
+        .window_setup(WindowSetup::default().title("Settlers of Lore"))
+        .window_mode(WindowMode::default().dimensions(1024, 720))
         .build()
         .unwrap();
 
-    let assets = find_folder::Search::ParentsThenKids(3, 3)
-        .for_folder("assets")
-        .unwrap();
-
-    let freetype = freetype::Library::init().unwrap();
-    let font = assets.join("m5x7.ttf");
-    let fontface = freetype.new_face(&font, 0).unwrap();
-    fontface.set_pixel_sizes(0, 48).unwrap();
-
-    let ref mut gl = GlGraphics::new(opengl);
-    let text = GlyphText::new(&fontface, "Hello Piston!".to_owned());
-
-    let mut events = Events::new(EventSettings::new().lazy(true));
-    while let Some(e) = events.next(&mut window) {
-        if let Some(args) = e.render_args() {
-            use graphics::{clear, color, Transformed};
-
-            gl.draw(args.viewport(), |c, gl| {
-                clear(color::WHITE, gl);
-                text.render(&c.trans(0.0, 27.0), gl);
-            });
-        }
+    if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        ctx.filesystem.mount(&path, true);
     }
+
+    let state = &mut MainState::new(ctx).unwrap();
+
+    event::run(ctx, state).unwrap();
 }
