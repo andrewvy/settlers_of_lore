bd572eec2e470baa163b56bd730cdb902fa4e70b
Andrew Vy <andrew@andrewvy.com>
2018-06-21 11:04:20 -0700

    :sparkles: Use compiled tree names

--- src/plantae/tree.rs
+++ src/plantae/tree.rs
@@ -1,3 +1,4 @@
+use std::str;
 use plantae::{Plantae, Quality};
 
 pub struct Tree {
@@ -5,15 +6,29 @@ pub struct Tree {
 }
 
 impl Tree {
-    pub fn new(id: u32, name: String) -> Plantae<Tree> {
+    pub fn new(id: u32, _name: String) -> Plantae<Tree> {
+        let generated_name = Tree::generate_name();
+
         Plantae {
             id,
-            name,
+            name: generated_name,
             ticks_per_growth: 120,
             max_growth_level: 50,
             inner: Tree {
                 has_bark: true,
             },
         }
+    }
+
+    pub fn generate_name() -> String {
+        let buffer = include_bytes!("../../resources/data/trees.txt");
+        let tree_names = match str::from_utf8(buffer) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+
+        let mut iter = tree_names.split('\n');
+
+        iter.nth(0).unwrap().to_owned()
     }
 }
